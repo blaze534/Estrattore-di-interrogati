07629ff9f6c782f24aeffb3e6acfd9da53db7414
blaze534 <86913853+blaze534@users.noreply.github.com>
2023-03-29 21:21:14 +0200

    V 1.1

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-#Moduli importati
+# Moduli importati
 import random
 import tkinter as tk
 from tkinter import messagebox
+
 numero = ""
-#Apertura File
+# Apertura File
 with open("lista.txt", "r") as f:
     lista = f.read().splitlines()
-#Definizione comando    
+
+
+def print_input():
+    print(input_field.get())
+
+
+def aggiungi_elemento():
+    global numero
+    if numero != "":
+        with open("lista.txt", "a") as f:
+            f.write(numero + "\n")
+    root.destroy()
+
+# Definizione comando
 def estrazione():
     global numero
     numero = random.choice(lista)
@@ -14,33 +28,55 @@ def estrazione():
     with open("lista.txt", "w") as f:
         for item in lista:
             f.write("%s\n" % item)
-    messagebox.showinfo("ESTRATTORE INTERROGAZIONI 3C ARTE", "è uscito "+ numero)
-#Comando annulla
+    messagebox.showinfo("ESTRATTORE INTERROGAZIONI 3C ARTE", "è uscito " + numero)
+
+
+# Comando annulla
 def annulla():
-  global numero
-  ultimo_estratto = numero
-  lista.append(ultimo_estratto)
-  with open("lista.txt", "a") as f:
-    f.write(ultimo_estratto + "\n")
-  messagebox.showinfo("Annullato",numero+ " riaggiunto alla lista!")
-  
+    global numero
+    ultimo_estratto = numero
+    lista.append(ultimo_estratto)
+    with open("lista.txt", "a") as f:
+        f.write(ultimo_estratto + "\n")
+    messagebox.showinfo("Annullato", numero + " riaggiunto alla lista!")
 
 
-#forma gui
+def elimina_elemento():
+    global numero
+    numero = input_field.get()
+    if numero in lista:
+        lista.remove(numero)
+        with open("lista.txt", "w") as f:
+            for item in lista:
+                f.write("%s\n" % item)
+        messagebox.showinfo("Eliminato", numero + " rimosso dalla lista!")
+        input_field.delete(0, 'end')
+    else:
+        messagebox.showerror("ERRORE", "Elemento non presente nella lista!")
+
+
+# forma gui
 root = tk.Tk()
 root.title("Estrattore v.1.0")
 root.geometry("300x200")
 
-
-#bottone per estrarre
-button = tk.Button(root, text="Clicca per estrarre", comman=estrazione)
+# bottone per estrarre
+button = tk.Button(root, text="Clicca per estrarre", command=estrazione)
 button.pack()
 
-#bottone Annulla
+# bottone Annulla
 undo_button = tk.Button(root, text="Annulla", command=annulla)
 undo_button.pack()
+label = tk.Label(root, text="Metti gli Assenti ")
+label.pack()
+input_field = tk.Entry(root)
+input_field.pack()
 
-label = tk.Label(root, text="Creato con </> da Biagio\n maggiori informazioni qui ")
+delete_button = tk.Button(root, text="Elimina gli assenti", command=elimina_elemento)
+delete_button.pack()
+
+label = tk.Label(root, text="Creato con </> da Biagio ")
 label.pack()
 
+root.protocol("WM_DELETE_WINDOW", aggiungi_elemento)
 root.mainloop()
